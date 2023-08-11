d36a09263fa18e92ea24a5cc41e0f4ac7240a7c9
FrodoNaveen <frodonaveen33@gmail.com>
2023-08-11 10:46:46 +0530

    js and css added to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,76 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
 import "./App.css";
+import React, { useState, useEffect, memo } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [displayName, setDisplayName] = useState("");
+
+  const [users, setUsers] = useState([]);
+
+  const API_URL = "https://api.github.com";
+
+  useEffect(() => {
+    fetch(`${API_URL}/search/users?q=${displayName}`)
+      .then((res) => res.json())
+      .then((data) => setUsers(data.items));
+  }, [displayName]);
+
+  function doSearch() {
+    setDisplayName(name);
+    setName("");
+  }
+
+  function handleChange(e) {
+    if (e.key === "Enter") {
+      setDisplayName(name);
+      setName("");
+    }
+  }
+
+  function openTab(id) {
+    window.open(id);
+  }
+
   return (
     <div className="App">
-      <h1>Naveen</h1>
+      <div>
+        <h1>
+          <i className="fa-brands fa-github"></i> GIT PROFILE FINDER
+        </h1>
+      </div>
+      <div className="container">
+        <input
+          onKeyUp={handleChange}
+          value={name}
+          className="input"
+          placeholder="Enter Github username"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button className="searchbtn" onClick={doSearch}>
+          Search
+        </button>
+      </div>
+      {users && users.length >= 1
+        ? users.map((ele, key) => (
+            <div key={ele.id} className="resultcontainer">
+              <img
+                className="avatar"
+                src={ele.avatar_url}
+                alt={`${ele.login} icon`}
+              />
+              <div className="userdetails">
+                <p>{ele.login}</p>
+                {/* // eslint-disable-next-line jsx-a11y/anchor-has-content */}
+                <p className="link" onClick={() => openTab(ele.html_url)}>
+                  <u style={{ color: "blue" }}>{ele.html_url}</u>
+                </p>
+              </div>
+            </div>
+          ))
+        : null}
     </div>
   );
 }
 
-export default App;
+export default memo(App);
